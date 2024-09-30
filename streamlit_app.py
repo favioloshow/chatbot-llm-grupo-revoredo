28418b937e3261822068b1b2dbafb1cc41d09436
favioccz <favio.cuya@tuxpas.com>
2024-09-30 15:08:44 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,56 +1,56 @@
 import streamlit as st
-from openai import OpenAI
+import requests
+import json
 
-# Show title and description.
-st.title("💬 Chatbot")
+# Mostrar título y descripción.
+st.title("💬 Chatbot Grupo Revoredo")
 st.write(
-    "This is a simple chatbot that uses OpenAI's GPT-3.5 model to generate responses. "
-    "To use this app, you need to provide an OpenAI API key, which you can get [here](https://platform.openai.com/account/api-keys). "
-    "You can also learn how to build this app step by step by [following our tutorial](https://docs.streamlit.io/develop/tutorials/llms/build-conversational-apps)."
+    "This is a simple chatbot that uses AWS cloud computing model to generate responses. "
+    "To use this app, you need to provide a key"
 )
 
-# Ask user for their OpenAI API key via `st.text_input`.
-# Alternatively, you can store the API key in `./.streamlit/secrets.toml` and access it
-# via `st.secrets`, see https://docs.streamlit.io/develop/concepts/connections/secrets-management
-openai_api_key = st.text_input("OpenAI API Key", type="password")
-if not openai_api_key:
-    st.info("Please add your OpenAI API key to continue.", icon="🗝️")
+lambda_url = "https://9h14dzfc80.execute-api.us-east-1.amazonaws.com/dev/query"
+token =  st.text_input("Token", type="password")
+
+
+if not token:
+    st.info("Por favor, ingresa el token de respuesta para continuar.", icon="🗝️")
 else:
-
-    # Create an OpenAI client.
-    client = OpenAI(api_key=openai_api_key)
-
-    # Create a session state variable to store the chat messages. This ensures that the
-    # messages persist across reruns.
+    # Crear una variable de estado de sesión para almacenar los mensajes.
     if "messages" not in st.session_state:
         st.session_state.messages = []
 
-    # Display the existing chat messages via `st.chat_message`.
+    # Mostrar los mensajes existentes.
     for message in st.session_state.messages:
         with st.chat_message(message["role"]):
             st.markdown(message["content"])
 
-    # Create a chat input field to allow the user to enter a message. This will display
-    # automatically at the bottom of the page.
-    if prompt := st.chat_input("What is up?"):
+    # Crear el campo de entrada para permitir al usuario escribir un mensaje.
+    prompt = st.chat_input("Escribe tu mensaje:")
 
-        # Store and display the current prompt.
+    if prompt:
+        # Almacenar y mostrar el mensaje del usuario.
         st.session_state.messages.append({"role": "user", "content": prompt})
         with st.chat_message("user"):
             st.markdown(prompt)
 
-        # Generate a response using the OpenAI API.
-        stream = client.chat.completions.create(
-            model="gpt-3.5-turbo",
-            messages=[
-                {"role": m["role"], "content": m["content"]}
-                for m in st.session_state.messages
-            ],
-            stream=True,
-        )
+        # Enviar la solicitud a la Lambda Function URL.
+        try:
+            response = requests.post(lambda_url, json={
+                "prompt": prompt,
+                "token": token
+                })
+            response.raise_for_status()  # Verificar si la solicitud fue exitosa
+            response_data = response.json()['body']
+            response_data = json.loads(response_data)
+            print(response_data)
+            # Verificar si la respuesta tiene el campo 'response'
+            lambda_response = response_data.get("generation", "No se recibió una respuesta válida de Lambda.")
 
-        # Stream the response to the chat using `st.write_stream`, then store it in 
-        # session state.
-        with st.chat_message("assistant"):
-            response = st.write_stream(stream)
-        st.session_state.messages.append({"role": "assistant", "content": response})
+            # Mostrar la respuesta de la Lambda en el chat.
+            with st.chat_message("assistant"):
+                st.markdown(lambda_response)
+            st.session_state.messages.append({"role": "assistant", "content": lambda_response})
+
+        except requests.exceptions.RequestException as e:
+            st.error(f"Error al llamar a la Lambda: {e}")
